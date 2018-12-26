cd2af79c41a8ff15f07553f8649095215e527a6f
madprogramer <themadprogramer@gmail.com>
2018-12-26 05:42:33 +0300

    URL extractor bug fixed
    More general now

--- saveTheShow/VideoIDHelper.py
+++ saveTheShow/VideoIDHelper.py
@@ -1,25 +1,29 @@
+import re
 def idExtractor(s):
-	#URL?
-	if isURL(s):
-		return getIDfromURL(s)
-	#Short URL?
-	elif isShortURL(s):
-		return getIDfromShortURL(s)
-	#Assume ID
-	else:
-		return s
+    #URL?
+    if isURL(s):
+        return getIDfromURL(s)
+    #Short URL?
+    elif isShortURL(s):
+        return getIDfromShortURL(s)
+    #Assume ID
+    else:
+        return s
 
 def isURL(s): 
-	return (s.find("www.youtube.com") != -1)
+    return (s.find("www.youtube.com") != -1)
 def isShortURL(s):
-	return (s.find("youtu.be") != -1)
+    return (s.find("youtu.be") != -1)
 
 
-def getIDfromURL(s): 
-	if(s.find("&")!=-1):
-		return  s[ s.find("v=")+2 :  s.find("&")] 
-	return  s[ s.find("v=")+2 :  ] 
+def getIDfromURL(s):
+    try:
+        return re.findall(r'v=[^&]+',s)[0]
+    except:
+        return None
+    
 def getIDfromShortURL(s):
-	if(s.find("?")!=-1):
-		return  s[ s.find("/", s.find("/")+2)+1 :  s.find("?")] 
-	return  s[ s.find("/", s.find("/")+2)+1  :  ]
+    if(s.find("?")!=-1):
+        return  s[ s.find("/", s.find("/")+2)+1 :  s.find("?")] 
+    return  s[ s.find("/", s.find("/")+2)+1  :  ]
+
